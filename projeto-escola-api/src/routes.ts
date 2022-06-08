33005e1b7dfe55c6c67892fbd8759d0d31dad0d8
N14R56 <2.0turbocoder@gmail.com>
2022-06-07 21:45:14 -0300

    feature: /test route created

--- projeto-escola-api/src/routes.ts
+++ projeto-escola-api/src/routes.ts
@@ -1,7 +1,10 @@
 import { Router } from "express";
 import { LoginController } from "./controllers/Login";
 const router = Router()
-router.post("/login/student", function(req, res){ new LoginController(req, res).student() })
+router.get("/test", function(req, res){ res.status(200).json({
+    'message' : 'Working, tested!'
+}) })
+router.post("/login/student", function(req, res){ new LoginController(req, res, 'Students Login').student() })
 router.post("/login/teacher")
 router.post("/login/parent")
 router.post("/login/administrator")
