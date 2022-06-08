1f12b7e3a5cf52624b37fbc779f63d1e024a807c
N14R56 <2.0turbocoder@gmail.com>
2022-06-07 21:52:11 -0300

    fix: Hostname 0.0.0.0 added. Wasn't able to curl to localhost ptoperly.

--- projeto-escola-api/src/server.ts
+++ projeto-escola-api/src/server.ts
@@ -1,8 +1,7 @@
-
 import express from "express";
 import { router } from "./routes";
 
 const app = express();
 app.use(express.json());
 app.use(router);
-app.listen(3333, () => { console.log("Running on port 3333") });
+app.listen(3333, '0.0.0.0', () => { console.log("Running on port 3333") });
